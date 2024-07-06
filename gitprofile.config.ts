68b2cdc0dae859e985c995af829d1811c4323b15
Bilal Haneef <57318407+MuhammadBilal848@users.noreply.github.com>
2024-07-07 03:50:58 +0500

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -142,7 +142,7 @@ const CONFIG = {
   certifications: [
     {
       name: 'Machine Learning Specialization',
-      // body: 'Lorem ipsum dolor sit amet',
+      body: 'This course provides a broad introduction to modern machine learning, including supervised learning (multiple linear regression, logistic regression, neural networks, and decision trees), unsupervised learning (clustering, dimensionality reduction, recommender systems), and some of the best practices used in Silicon Valley for artificial intelligence and machine learning innovation (evaluating and tuning models, taking a data-centric approach to improving performance, and more.)',
       year: 'February 8, 2023',
       link: 'https://www.coursera.org/account/accomplishments/specialization/XJP4NXRVSKLV',
     },
