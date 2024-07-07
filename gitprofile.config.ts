f8c4c6b81ca260585c1c4182d566654d7fa7a598
Bilal Haneef <57318407+MuhammadBilal848@users.noreply.github.com>
2024-07-08 00:33:11 +0500

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -232,7 +232,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'dim',
+    defaultTheme: 'lofi',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
